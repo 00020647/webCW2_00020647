b6ca934bba1cb180cc0b400e16e087467bb1e9c0
Sam <sbekmuratov2@students.wiut.uz>
2025-04-06 16:05:18 +0500

    Working prototype

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Importing dependecies 
 const express = require('express')
 const path = require('path')
 
@@ -8,22 +9,29 @@ const port = 3000
 
 const app = express()
 
+//Defining the path to the mock database
 global.mock_db = path.join(__dirname, './data/mock_db.json')
 
+//Middleware to parse incoming requests 
 app.use(express.json())
 app.use(express.urlencoded({
     extended: false
 }))
 
+//View Engine
+//pug location
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+//importing the CSS and JS files
 app.use('/css', express.static('public/css'))
 app.use('/js', express.static('public/js'))
 
+//Importing API and web routes
 app.use('/api', apiRoutes)
 app.use('/', webRoutes)
 
+//Starting the server
 app.listen(port, ()=>{
     console.log(`server running at http://localhost:${port}/flashcard`)
 })
